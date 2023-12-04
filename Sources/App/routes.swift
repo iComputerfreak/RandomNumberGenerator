04ae245f166135bdb2e9f8bbb9e6fad0df4d9ae5
Jonas Frey <dev@jonasfrey.de>
2023-12-04 19:04:51 +0100

    Fixed crash when entering an invalid range

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,20 +1,26 @@
 import Vapor
 
 enum ParameterError: Swift.Error {
-    case invalidQuery(String)
+    case invalidRange(min: Int, max: Int)
 }
 
 struct RandomIntQuery: Content {
     let min: Int
     let max: Int
     var result: Int {
-        Int.random(in: min...max)
+        get throws {
+            guard min <= max else {
+                throw ParameterError.invalidRange(min: min, max: max)
+            }
+            return Int.random(in: min...max)
+        }
     }
 }
 
 func routes(_ app: Application) throws {
     app.get { (req) async throws -> View in
         let query = try req.query.decode(RandomIntQuery.self)
+        req.logger.info("Generating a random integer between \(query.min) and \(query.max)")
         return try await req.view.render("index", ["min": query.min, "max": query.max, "result": query.result])
     }
 
