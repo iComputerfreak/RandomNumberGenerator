81362dd95d8e952adde71485be644ad83987cc6f
Jonas Frey <dev@jonasfrey.de>
2023-12-04 19:49:58 +0100

    Fixed too small font size on mobile devices Added error messages to frontend and help text when calling the url without query parameters

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -17,11 +17,38 @@ struct RandomIntQuery: Content {
     }
 }
 
+struct Payload: Content {
+    let min: Int
+    let max: Int
+    let result: Int?
+    let error: String?
+    
+    init(from query: RandomIntQuery) {
+        self.min = query.min
+        self.max = query.max
+        do {
+            self.result = try query.result
+            self.error = nil
+        } catch ParameterError.invalidRange(min: _, max: _) {
+            self.error = "Invalid Range"
+            self.result = nil
+        } catch {
+            self.error = "Error"
+            self.result = nil
+        }
+    }
+}
+
 func routes(_ app: Application) throws {
     app.get { (req) async throws -> View in
-        let query = try req.query.decode(RandomIntQuery.self)
-        req.logger.info("Generating a random integer between \(query.min) and \(query.max)")
-        return try await req.view.render("index", ["min": query.min, "max": query.max, "result": query.result])
+        do {
+            let query = try req.query.decode(RandomIntQuery.self)
+            req.logger.info("Generating a random integer between \(query.min) and \(query.max)")
+            let payload = Payload(from: query)
+            return try await req.view.render("index", payload)
+        } catch {
+            return try await req.view.render("help")
+        }
     }
 
     app.get("hello") { req async -> String in
